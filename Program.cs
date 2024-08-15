94bea2f3aed59c53b98209d2841cc8280fa6d307
Jeff Balbalosa <cyjeffb@yahoo.com>
2024-08-15 15:42:53 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Simple Array!");
+
+int num1 = 0;
+int num2 = 0;
+int num3 = 0;
+int num4 = 0;
+int num5 = 0;
+
+//declare an array
+int[] myIntArray = new int[5];
+
+myIntArray[0] = 5;  
+myIntArray[1] = 12;  
+myIntArray[2] = 45;
+myIntArray[3] = 98;
+myIntArray[4] = 7;
+
+Console.WriteLine($"{myIntArray[3]}");
+
+
+Console.ReadKey();
